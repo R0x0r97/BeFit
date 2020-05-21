c73e4d8a9b28462e350167cb1cbf92bb16111734
okpeter97 <peter.olahkatai@gmail.com>
2020-05-21 17:48:14 +0300

    .

---
+++ BeFit/BeFit.Logic/BeFitController.cs
@@ -0,0 +1,25 @@
+﻿// ------------------------------------------------------------------------------------------------------------------
+
+
+namespace BeFit.Logic
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using BeFit.Model;
+    using BeFit.Model.DBContext;
+
+    public class BeFitController
+    {
+        private BeFitDB beFitDatabase;
+
+        public BeFitController()
+        {
+            this.beFitDatabase = new BeFitDB();
+        }
+
+        public List<User> GetUsers()
+        {
+            return beFitDatabase.Users.ToList();
+        }
+    }
+}
